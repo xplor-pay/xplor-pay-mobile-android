525c01ac38c4149c27258f25fe6763641ddc1380
Victor Boieru <Octavian.Boieru@endava.com>
2022-04-18 17:04:32 +0300

    add standalone app with tab bar

---
+++ app/src/main/java/com/xplore/paymobile/ui/transactions/TransactionsViewModel.kt
@@ -0,0 +1,13 @@
+package com.xplore.paymobile.ui.transactions
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TransactionsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is transactions Fragment"
+    }
+    val text: LiveData<String> = _text
+}
