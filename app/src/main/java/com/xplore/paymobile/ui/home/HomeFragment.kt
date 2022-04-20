0b459595725cf3df6a7522e57547aaa0326ffe76
adrianrusu16 <72539388+adrianrusu16@users.noreply.github.com>
2022-04-20 11:54:42 +0300

    removed mocked data

--- app/src/main/java/com/xplore/paymobile/ui/home/HomeFragment.kt
+++ app/src/main/java/com/xplore/paymobile/ui/home/HomeFragment.kt
@@ -57,7 +57,7 @@ class HomeFragment : Fragment() {
     private fun setListeners() {
         binding.apply {
             chargeButton.setOnClickListener {
-                viewModel.cycleReaders()
+                // Handle button press
             }
             chargeCounter.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
--- app/src/main/java/com/xplore/paymobile/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/xplore/paymobile/ui/home/HomeViewModel.kt
@@ -1,50 +1,16 @@
 package com.xplore.paymobile.ui.home
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.xplore.paymobile.model.BatteryLifeState
-import com.xplore.paymobile.model.Reader
 import com.xplore.paymobile.model.ReaderState
-import com.xplore.paymobile.model.SignalState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
-    private var readerIndex = 0
-
-    private val readers = listOf(
-        ReaderState.NoReader,
-        ReaderState.ReaderIdle(Reader("1", "My reader")),
-        ReaderState.ReaderPaired(
-            Reader("1", "My reader"),
-            SignalState.Weak,
-            BatteryLifeState(97)
-        ),
-        ReaderState.ReaderPaired(
-            Reader("2", "Mircea's reader"),
-            SignalState.Weak,
-            BatteryLifeState(93)
-        ),
-        ReaderState.ReaderPaired(
-            Reader("3", "Macara's reader"),
-            SignalState.Good,
-            BatteryLifeState(18)
-        ),
-        ReaderState.ReaderPaired(
-            Reader("4", "Marcel's reader"),
-            SignalState.Weak,
-            BatteryLifeState(55)
-        ),
-    )
-
-    private val _readerState = MutableStateFlow(readers[readerIndex])
-    val readerState: StateFlow<ReaderState> = _readerState
-
-    fun cycleReaders() = viewModelScope.launch {
-        readerIndex++
-        readerIndex %= readers.size
-        _readerState.emit(readers[readerIndex])
+    companion object {
+        private val defaultReaderState = ReaderState.NoReader
     }
+
+    private val _readerState = MutableStateFlow(defaultReaderState)
+    val readerState: StateFlow<ReaderState> = _readerState
 }
