57a52a5af2bf0e94c9125b01e1decc2afe5414f8
victorboieru <Octavian.Boieru@endava.com>
2022-12-13 10:43:43 +0200

    create info page

--- app/src/main/java/com/xplore/paymobile/ui/info/InfoFragment.kt
+++ app/src/main/java/com/xplore/paymobile/ui/info/InfoFragment.kt
@@ -1,11 +1,15 @@
 package com.xplore.paymobile.ui.info
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.xplore.paymobile.BuildConfig
+import com.xplore.paymobile.R
 import com.xplore.paymobile.databinding.FragmentInfoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +21,8 @@ class InfoFragment : Fragment() {
     private var _binding: FragmentInfoBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var clientSupportPhoneNumber: String
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,6 +30,48 @@ class InfoFragment : Fragment() {
     ): View {
         _binding = FragmentInfoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        clientSupportPhoneNumber = getString(R.string.client_support_phone_number)
+        setupTextViews()
+        setupClickListeners()
+    }
+
+    private fun setupTextViews() {
+        binding.apply {
+            callMessage.text = getString(R.string.info_screen_call_message, clientSupportPhoneNumber)
+            appVersion.text = getString(R.string.info_screen_app_version, BuildConfig.VERSION_NAME)
+        }
+    }
+
+    private fun setupClickListeners() {
+        binding.apply {
+            callButton.setOnClickListener {
+                callClientSupport()
+            }
+            termsAndConditionsButton.setOnClickListener {
+                openTermsAndConditionsLink()
+            }
+            logOutButton.setOnClickListener {
+                // TODO to be implemented
+            }
+        }
+    }
+
+    private fun callClientSupport() {
+        val phoneIntent = Intent(Intent.ACTION_CALL)
+        phoneIntent.data = Uri.parse("tel:" + clientSupportPhoneNumber)
+        startActivity(phoneIntent)
+    }
+
+    private fun openTermsAndConditionsLink() {
+        val url = getString(R.string.terms_and_conditions_link)
+        val linkIntent = Intent(Intent.ACTION_VIEW)
+        linkIntent.data = Uri.parse(url)
+        startActivity(linkIntent)
     }
 
     override fun onDestroyView() {
