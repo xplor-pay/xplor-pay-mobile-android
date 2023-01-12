c687936d64ca9d5a32d4f5dd340bc640e6849d1a
Mihai Rosu <mihai.rosu@endava.com>
2023-01-12 13:55:14 +0200

    Changes for review discussions

--- app/src/main/java/com/xplore/paymobile/ui/settings/SettingsViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.xplore.paymobile.ui.settings
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class SettingsViewModel @Inject constructor(): ViewModel() {
-}
